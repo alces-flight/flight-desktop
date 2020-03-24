f235d148735580c9f1392c9994e145ef0a14332d
Mark J. Titorenko <mark.titorenko@alces-flight.com>
2020-03-24 18:28:19 +0000

    Bump to 1.3.0-rc1

--- lib/desktop/version.rb
+++ lib/desktop/version.rb
@@ -25,5 +25,5 @@
 # https://github.com/alces-flight/flight-desktop
 # ==============================================================================
 module Desktop
-  VERSION = '1.3.0-dev'
+  VERSION = '1.3.0-rc1'
 end
