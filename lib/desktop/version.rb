e5338f547b984c8b21510aee4649b4e782df66e9
Ben Armston <ben.armston@alces-software.com>
2022-05-19 19:15:07 +0100

    Bump version

--- lib/desktop/version.rb
+++ lib/desktop/version.rb
@@ -25,5 +25,5 @@
 # https://github.com/alces-flight/flight-desktop
 # ==============================================================================
 module Desktop
-  VERSION = '1.10.0-rc2'
+  VERSION = '1.10.0'
 end
