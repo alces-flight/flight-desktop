dc957eacf0d11f89c607d8e63b9665beb62c15f5
VoxSecundus <millard64@hotmail.co.uk>
2022-08-25 15:40:51 +0100

    Bump version

--- lib/desktop/version.rb
+++ lib/desktop/version.rb
@@ -25,5 +25,5 @@
 # https://github.com/alces-flight/flight-desktop
 # ==============================================================================
 module Desktop
-  VERSION = '1.11.2'
+  VERSION = '1.11.3'
 end
