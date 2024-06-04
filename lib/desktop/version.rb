82db996842285d9539b2190c672524522414d725
Jack Millard <millard64@hotmail.co.uk>
2024-06-04 14:42:46 +0100

    Bump version

--- lib/desktop/version.rb
+++ lib/desktop/version.rb
@@ -25,5 +25,5 @@
 # https://github.com/alces-flight/flight-desktop
 # ==============================================================================
 module Desktop
-  VERSION = '1.11.5'
+  VERSION = '1.11.6'
 end
